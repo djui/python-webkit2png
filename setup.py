dca9c161570a7c034bcd29230ffdbd9a7a14814b
Uwe Dauernheim <uwe@dauernheim.net>
2016-02-11 10:04:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description = long_description,
     author = 'Roland Tapken',
     author_email = 'roland at dau-sicher de',
-    packages = find_packages(['webkit2png']),
+    packages = find_packages(),
     zip_safe=True,
     include_package_data=True,
     package_dir = [],
